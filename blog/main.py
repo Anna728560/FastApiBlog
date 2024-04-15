d84c530e055de6cd98069a9373dd51ec9cc01f69
Anya <xxxuko@gmail.com>
2024-04-15 21:33:19 +0300

    create blog create endpoint

---
+++ blog/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+from blog.schemas import Blog
+
+app = FastAPI()
+
+
+@app.post("/blog/")
+def create_blog(blog: Blog):
+    return {"data": f"Blog is created with title as {blog.title}"}
+
+
