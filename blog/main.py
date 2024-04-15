a09febfd2eb363d7e9d8b5d090b44727d5f798e5
Anya <xxxuko@gmail.com>
2024-04-15 21:49:08 +0300

    add tablename for Blog model

--- blog/main.py
+++ blog/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 
-from blog.schemas import Blog
+from blog import models, schemas
+from blog.database import engine
 
 app = FastAPI()
 
+models.Base.metadata.create_all(engine)
+
 
 @app.post("/blog/")
-def create_blog(blog: Blog):
+def create_blog(blog: schemas.Blog):
     return {"data": f"Blog is created with title as {blog.title}"}
--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from blog.database import Base
 
 
 class Blog(Base):
+    __tablename__ = "blogs"
+
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
